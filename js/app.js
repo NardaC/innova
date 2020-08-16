8d815c663a5fde4cb8960320f359b4bde1f71b99
NardaC <nardac6@gmail.com>
2020-08-16 04:26:59 -0500

    app

--- js/app.js
+++ js/app.js
@@ -1,9 +1,29 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
 
-	jQuery(".hamb").click(function(e){
-	 	e.preventDefault();
-	 	jQuery("header .background-header").toggleClass("abrir");
-	 	jQuery(".hamb i").toggleClass("fa-times");
-	});
+    jQuery(".hamb").click(function (e) {
+        e.preventDefault();
+        jQuery("header .background-header").toggleClass("abrir");
+        jQuery(".hamb i").toggleClass("fa-times close-menu");
+        jQuery("#inicio").hover(function (e) {
+            e.preventDefault();
+            jQuery(this).addClass("hover-activo");
+            jQuery("#nosotros").removeClass("hover-activo");
+            jQuery(".img-header-nav2").removeClass("img-header-nav2");
+            jQuery("#box-img-header").addClass("img-header-nav ");
+
+
+        });
+        jQuery("#nosotros").hover(function (e) {
+            e.preventDefault();
+            jQuery(this).addClass("hover-activo");
+            jQuery("#inicio").removeClass("hover-activo");
+            jQuery(".img-header-nav").removeClass("img-header-nav");
+            jQuery("#box-img-header").addClass("img-header-nav2");
+
+        });
+
+    });
+
+
 
 });
